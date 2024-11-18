f29f12dfb3ca2828c8abf09cab6fa0620231c4ba
Danielle Yahalom <danielleyahalom@Danielles-MacBook-Air-9.local>
2024-11-17 19:53:41 -0500

    fixed bug

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="127.0.0.1",
-        port=8000,
-        reload=True,
-        timeout_keep_alive=1020  # Increase timeout to 120 seconds (or any value you prefer)
+        port=5000,
+        reload=True
     )
+   
